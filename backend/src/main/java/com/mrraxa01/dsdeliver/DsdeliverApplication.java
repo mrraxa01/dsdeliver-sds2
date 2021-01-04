b96548b57ab3d26e60f8ad41d468d3f1d2efe4e2
mrraxa01 <mrraxa01@gmail.com>
2021-01-04 19:35:45 -0300

    Project Created

---
+++ backend/src/main/java/com/mrraxa01/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.mrraxa01.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mrraxa01/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrraxa01.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
