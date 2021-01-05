9fb5ae3259bbce69612e36c7143bdc8159ba5d39
mrraxa01 <mrraxa01@gmail.com>
2021-01-05 00:33:35 -0300

    POST /orders

--- backend/src/main/java/com/mrraxa01/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/mrraxa01/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.mrraxa01.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mrraxa01.dsdeliver.dto.OrderDTO;
 import com.mrraxa01.dsdeliver.services.OrderService;
@@ -24,5 +28,12 @@ public class OrderController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id})")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 
 }
--- backend/src/main/java/com/mrraxa01/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/mrraxa01/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.mrraxa01.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,21 +9,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mrraxa01.dsdeliver.dto.OrderDTO;
+import com.mrraxa01.dsdeliver.dto.ProductDTO;
 import com.mrraxa01.dsdeliver.entities.Order;
+import com.mrraxa01.dsdeliver.entities.OrderStatus;
+import com.mrraxa01.dsdeliver.entities.Product;
 import com.mrraxa01.dsdeliver.repositories.OrderRepository;
+import com.mrraxa01.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){	
 		
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-		
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+			}
+			order = repository.save(order);
+			return new OrderDTO(order);
 		
 	}
 }
