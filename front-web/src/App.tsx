f8b592f7e934c2524f8d6a35312b3ca0af87e415
mrraxa01 <mrraxa01@gmail.com>
2021-01-06 14:56:50 -0300

    Add Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div >
       <Navbar />
+      <Home />
     </div>
   );
 }
