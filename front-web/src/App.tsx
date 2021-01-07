6d4f445f14270831637f9d1c51e07ef5772d60e9
mrraxa01 <mrraxa01@gmail.com>
2021-01-06 23:27:47 -0300

    Add routes and steps

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,18 +1,13 @@
 
 import './App.css';
-import Footer from './Footer';
-import Home from './Home';
-import Navbar from './Navbar';
-
+import Routes from './Routes';
 
 
 function App() {
   return (
-    <div >
-      <Navbar />
-      <Home />
-        
-    </div>
+ 
+      <Routes />
+
   );
 }
 
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -10,22 +11,22 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">
                             Faça seu pedido <br /> que entregamos <br /> para você!!!
-               </h1>
+                         </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />levaremos na sua porta
-               </h3>
-                        <a href="orders" className="home-btn-order">
+                        </h3>
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-               </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
                     </div>
 
                 </div>
-                <Footer />
+                
             </div>
-
+            <Footer />
         </>
     );
 
